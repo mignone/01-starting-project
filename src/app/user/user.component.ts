ed525d379d172bebfbd8cff9b98a4f4de110b2d2
viktoriaviszlai@gmail.com <viktoriaviszlai@gmail.com>
2025-02-02 21:03:25 +0100

    .

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,4 +1,4 @@
-import { Component, computed, Input, input } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 
 @Component({
   selector: 'app-user',
@@ -8,10 +8,14 @@ import { Component, computed, Input, input } from '@angular/core';
   styleUrl: './user.component.css'
 })
 export class UserComponent {
+  @Input({required: true}) id!: string;
   @Input({required: true}) avatar!: string;
   @Input({required: true}) name!: string;
+  @Output() select = new EventEmitter<string>();
 
   get imagePath() { return 'assets/users/' + this.avatar; }
 
-  onSelectUser() {}
+  onSelectUser() {
+    this.select.emit(this.id);
+  }
 }
